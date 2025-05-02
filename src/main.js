57c2c77d8a794f2a0971afe914f1e7a3b88c6686
Mohamedabdullhaleam <mohamedabdullhaleam236@gmail.com>
2025-05-02 16:23:53 +0300

    session 1

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { Observable } from "rxjs";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// const promise = new Promise((resolve, reject) => {
+//   resolve();
+// });
+// promise.then(() => {
+//   console.log("promise resolved");
+// });
 
-setupCounter(document.querySelector('#counter'))
+// const observable = new Observable(callBack);
+// observable.subscribe();
+
+// function callBack() {
+//   console.log("haleem speaking");
+// }
+
+const observable = new Observable((obs) => {
+  obs.next(1);
+  obs.next(2);
+});
+
+console.log("oneeee");
+observable.subscribe((val) => {
+  console.log(val);
+});
+console.log("twoooo");
